c4c258b37fb7b867ceeb86e0517623109efa9a6b
Adegbite Ayoade <tripleaceme@gmail.com>
2024-07-16 02:42:37 +0100

    Blue Bikes Pipeline

--- blue_bikes_pipeline/dbt_blue_bikes/models/analysis/Customer_Segmentation/category_type.sql
+++ blue_bikes_pipeline/dbt_blue_bikes/models/analysis/Customer_Segmentation/category_type.sql
@@ -1,10 +1,12 @@
 with category_type as(
+    
     select member_casual, category, 
     sum(pricing) as total_rev,
     count(*) total_rides
     from {{ ref('insight_data') }}
     group by 1, 2
 )
+
 
 select *
 from category_type
